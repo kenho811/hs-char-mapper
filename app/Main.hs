35b8516dabafab7acb6413e6987e7c7c88791167
ken.ho <ken.ho@fdmgroup.com>
2021-10-31 21:36:18 +0800

    separated GUI and CLI development

--- app/Main.hs
+++ app/Main.hs
@@ -4,7 +4,9 @@
 module Main where
 
 import qualified CLI.Cli as Cli
+import qualified GUI.Gui as Gui
 
 
 main :: IO ()
-main = Cli.runCli
+-- main = Cli.runCli
+main = Gui.runGui
--- src/GUI/Gui.hs
+++ src/GUI/Gui.hs
@@ -1,5 +1,29 @@
+{-# LANGUAGE OverloadedLabels #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module GUI.Gui where
 
-hello :: Int 
-hello = 1
+import Data.GI.Base
+import qualified GI.Gtk as Gtk
 
+runGui :: IO ()
+runGui = do
+  Gtk.init Nothing
+
+  win <- new Gtk.Window [#title := "Character Converter in Haskell"]
+  on win #destroy Gtk.mainQuit
+  #resize win 640 480
+
+  box <- new Gtk.Box [ #orientation := Gtk.OrientationVertical ]
+  #add win box
+
+  msg <- new Gtk.Label [ #label := "Hello"]
+  #packStart box msg True False 10
+
+  btn <- new Gtk.Button [ #label := "Click me!" ]
+  #packStart box btn False False 10
+  on btn #clicked (set msg [ #label := "Clicked!"])
+
+  #showAll win
+
+  Gtk.main
